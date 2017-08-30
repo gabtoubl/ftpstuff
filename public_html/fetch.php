fef6aaed0c47d63d11a2cf9eeb4c597658815d45
Gabriel Toublanc <gabriel@sportlogiq.com>
2017-08-30 11:09:32 -0400

    Lot more asian store and brand

--- public_html/fetch.php
+++ public_html/fetch.php
@@ -20,10 +20,7 @@ function cancube($dom, $page = 0) {
       $productName = $xpath->query(".//*[contains(@class, 'product-card__name')]", $product)[0]->nodeValue;
       $productPrice = $xpath->query(".//*[contains(@class, 'product-card__price')]", $product)[0];
       $productPrice = floatval(substr($xpath->query(".//*[contains(@class, 'money')]", $productPrice)[0]->nodeValue, 1));
-      $puzzles[] = array('name' => $productName,
-                         'price' => $productPrice,
-                         'currency' => 'CAD',
-                         'store' => 'cancube');
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'CAD', 'store' => 'cancube');
     }
     echo json_encode($puzzles);
   }
@@ -47,13 +44,10 @@ function cubingoutloud($dom, $page = 0) {
     $puzzles = [];
     foreach ($products as $product) {
       $productName = $xpath->query(".//*[contains(@class, 'grid-link__title')]", $product)[0]->nodeValue;
-      $productName = preg_replace('/[^a-z0-9\-\']/i', ' ', $productName);
+      $productName = preg_replace('/[^a-z0-9\-\' ]/i', '', $productName);
       $productPrice = $xpath->query(".//*[contains(@class, 'grid-link__meta')]", $product)[0];
       $productPrice = floatval(substr($product->getElementsByTagName('strong')[0]->nodeValue, 1));
-      $puzzles[] = array('name' => $productName,
-                         'price' => $productPrice,
-                         'currency' => 'CAD',
-                         'store' => 'cubingoutloud');
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'CAD', 'store' => 'cubingoutloud');
     }
     echo json_encode($puzzles);
   }
@@ -79,10 +73,7 @@ function speedcubescanada($dom, $page = 0) {
       $productName = $product->getElementsByTagName('p')[0]->nodeValue;
       $productPrice = $xpath->query(".//*[contains(@class, 'product-item--price')]", $product)[0];
       $productPrice = floatval(substr(explode(' ', $xpath->query(".//*[contains(@class, 'money')]", $productPrice)[0]->nodeValue)[0], 1));
-      $puzzles[] = array('name' => $productName,
-                         'price' => $productPrice,
-                         'currency' => 'CAD',
-                         'store' => 'speedcubescanada');
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'CAD', 'store' => 'speedcubescanada');
     }
     echo json_encode($puzzles);
   }
@@ -109,10 +100,121 @@ function cubezz($dom, $page = 0) {
       $productName = $productName->getElementsByTagName('a')[0]->nodeValue;
       $productPrice = $xpath->query(".//*[contains(@class, 'gaddprice')]", $product)[0];
       $productPrice = floatval(explode(' ', $xpath->query(".//*[contains(@class, 'my_shop_price')]", $productPrice)[0]->nodeValue)[1]);
-      $puzzles[] = array('name' => $productName,
-                         'price' => $productPrice,
-                         'currency' => 'USD',
-                         'store' => 'cubezz');
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'cubezz');
+    }
+    echo json_encode($puzzles);
+  }
+}
+
+function hknowstore($dom, $page = 0) {
+  $url = 'http://www.hknowstore.com/locale/en-EN/category/All%20Puzzles?sortby=price&dir=1';
+  if ($page != 0)
+    $url .= "&page=".$page;
+  $dom->LoadHTML(file_get_contents($url));
+  $xpath = new DOMXPath($dom);
+
+  if ($page == 0) {
+    $pagination = $dom->getElementById('ctl01_PageContent_ItemTable_pager_ctl00_lblPager')->nodeValue;
+    preg_match('/in (\d+) pages/', $pagination, $matches);
+    $lastPage = $matches[1];
+    echo $lastPage;
+  }
+  else {
+    $products = $xpath->query("//*[contains(@class, 'ItemGridLeft')]");
+    $puzzles = [];
+    foreach ($products as $product) {
+      $productName = trim($xpath->query(".//*[contains(@class, 'itemNameSpan')]", $product)[0]->nodeValue);
+      $productPrice = $xpath->query(".//*[contains(@class, 'unitPriceSpan')]", $product)[0]->nodeValue;
+      $productPrice = floatval(explode(' ', $productPrice)[1]);
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'hknowstore');
+    }
+    echo json_encode($puzzles);
+  }
+}
+
+function championscubestore($dom, $page = 0) {
+  $url = 'http://www.championscubestore.com/index.php?main_page=products_all&disp_order=4';
+  if ($page != 0)
+    $url .= "&page=".$page;
+  $dom->LoadHTML(file_get_contents($url));
+  $xpath = new DOMXPath($dom);
+
+  if ($page == 0) {
+    $pagination = $dom->getElementById('allProductsListingTopNumber');
+    $nodes = $pagination->getElementsByTagName('strong');
+    $lastPage = ceil($nodes[2]->nodeValue / $nodes[1]->nodeValue);
+    echo $lastPage;
+  }
+  else {
+    $puzzles = [];
+    $products = $xpath->query("//*[contains(@class, 'productListing-odd')]");
+    foreach ($products as $product) {
+      $productName = $xpath->query(".//*[contains(@class, 'name')]", $product)[0]->getElementsByTagName('strong')[0]->nodeValue;
+      $productName = preg_replace('/[^a-z0-9\-\' ]/i', '', $productName);
+      $productPrice = $xpath->query(".//*[contains(@class, 'price')]", $product)[0]->nodeValue;
+      $productPrice = floatval(str_replace(',', '', substr($productPrice, 1)));
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'championscubestore');
+    }
+    $products = $xpath->query("//*[contains(@class, 'productListing-even')]");
+    foreach ($products as $product) {
+      $productName = $xpath->query(".//*[contains(@class, 'name')]", $product)[0]->getElementsByTagName('strong')[0]->nodeValue;
+      $productName = preg_replace('/[^a-z0-9\-\' ]/i', '', $productName);
+      $productPrice = $xpath->query(".//*[contains(@class, 'price')]", $product)[0]->nodeValue;
+      $productPrice = floatval(str_replace(',', '', substr($productPrice, 1)));
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'championscubestore');
+    }
+    echo json_encode($puzzles);
+  }
+}
+
+function cube4you($dom, $page = 0) {
+  $url = 'http://cube4you.com/products_all.html?disp_order=4';
+  if ($page != 0)
+    $url .= "&page=".$page;
+  $dom->LoadHTML(file_get_contents($url));
+  $xpath = new DOMXPath($dom);
+
+  if ($page == 0) {
+    $pagination = $xpath->query("//*[contains(@class, 'navSplitPagesResult')]")[0];
+    $nodes = $pagination->getElementsByTagName('strong');
+    $lastPage = ceil($nodes[2]->nodeValue / $nodes[1]->nodeValue);
+    echo $lastPage;
+  }
+  else {
+    $puzzles = [];
+    $products = $xpath->query("//*[contains(@class, 'table-product-attributes')]");
+    foreach ($products as $product) {
+      $productName = $product->getElementsByTagName('strong')[0]->nodeValue;
+      $productPrice = $xpath->query(".//*[contains(@class, 'table-price')]", $product)[0]->nodeValue;
+      $productPrice = floatval(substr($productPrice, 1));
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'championscubestore');
+    }
+    echo json_encode($puzzles);
+  }
+}
+
+function zcube($dom, $page = 0) {
+  $url = 'http://www.zcube.hk/Brand?sort=3.1';
+  if ($page != 0)
+    $url .= "&page=".$page;
+  $dom->LoadHTML(file_get_contents($url));
+  $xpath = new DOMXPath($dom);
+
+  if ($page == 0) {
+    $pagination = $xpath->query("//*[contains(@class, 'col-sm-6 text-right')]")[0]->nodeValue;
+    preg_match('/\((\d+) Pages\)/', $pagination, $matches);
+    $lastPage = $matches[1];
+    echo $lastPage;
+  }
+  else {
+    $puzzles = [];
+    $products = $xpath->query("//*[contains(@class, 'col-sm-4')]");
+    print_r($products);
+    foreach ($products as $product) {
+      $productName = $product->getElementsByTagName('h4')[0]->nodeValue;
+      $productPrice = $xpath->query(".//*[contains(@class, 'price-new')]", $product)[0]->nodeValue;
+      $productPrice = floatval(substr($productPrice, 2));
+      $puzzles[] = array('name' => $productName, 'price' => $productPrice, 'currency' => 'USD', 'store' => 'championscubestore');
     }
     echo json_encode($puzzles);
   }
@@ -128,4 +230,3 @@ if (isset($_GET['site'])) {
 }
 
 ?>
-
